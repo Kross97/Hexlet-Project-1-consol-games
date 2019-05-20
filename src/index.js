b7fb69a84471a70a576a3568076142089432b867
Nikita Bezhenar <kross78080@yandex.ru>
2019-05-21 00:26:53 +0300

    1

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import  readlineSync from 'readline-sync';
+
+export default () => {
+
+const actual = readlineSync.question('May I have your name?: ');
+const text = `Hello,${actual}`;
+console.log(text);
+
+};
