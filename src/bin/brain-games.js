3b1818c4edb2e1149d3a551a5684759ab75e2bc8
Nikita Bezhenar <kross78080@yandex.ru>
2019-05-23 01:05:26 +0300

    step5

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,7 +1,35 @@
 
 import welcome from '..';
-// const welcome = require('..');
-
+import readlineSync from 'readline-sync';
+import even from './brain-even';
+import calc from './brain-calc';
+console.log('===========================');
 console.log('Welcome to the Brain Games!');
+const name = readlineSync.question('May I have your name?: ');
 
-welcome();
+welcome(name);
+
+
+let i;
+while (true) {
+  console.log('1.Brain-even');
+  console.log('2.Brain-calculyator');
+  console.log('9.Exit');
+  const number = readlineSync.question('Enter number game :' );
+
+if (number==1){
+  even(name);
+}
+
+if (number==2){
+  calc(name);
+}
+
+else if (number==9) {
+  console.log('Good bye');
+  console.log('Exit games....');
+  break;
+}
+
+i++;
+}
