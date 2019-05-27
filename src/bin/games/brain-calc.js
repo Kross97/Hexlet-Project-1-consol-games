40ad612298369dd8ce8bb569b857a835fd31e9cb
Nikita Bezhenar <kross78080@yandex.ru>
2019-05-27 20:54:42 +0300

    W1w

--- src/bin/games/brain-calc.js
+++ src/bin/games/brain-calc.js
@@ -5,59 +5,39 @@ export default (name) => {
   console.log('=====================================');
   console.log('What is the result of the expression?');
   const raunds = 3;
-  
+
   const arrOperation = ['+', '-', '*', '+', '-', '*', '+', '-', '*', '+'];
 
   for (let i = 0; i < raunds; i += 1) {
     const operationIndex = Math.floor(Math.random() * 10);
     const number1 = Math.floor(Math.random() * 10);
     const number2 = Math.floor(Math.random() * 10);
+
+    console.log(`Question: ${number1} ${arrOperation[operationIndex]} ${number2} `);
+
+    let result = 0;
+
     if (arrOperation[operationIndex] === '+') {
-      const sum = number1 + number2;
-      console.log(`Question: ${number1} + ${number2} `);
-
-      const answerStr = readlineSync.question('Enter answer:');
-      const answerNumber = parseInt(answerStr);
-
-      if (answerNumber === sum) {
-        console.log('Correct!');
-      } else {
-        console.log(`${answerNumber} is wrong answer ;(. Correct answer was ${sum}.`);
-        console.log(`Let's try again, ${name}!`);
-        break;
-      }
+      result = number1 + number2;
     }
 
     if (arrOperation[operationIndex] === '-') {
-      const sum = number1 - number2;
-      console.log(`Question: ${number1} - ${number2} `);
-
-      const answerStr = readlineSync.question('Enter answer:');
-      const answerNumber = parseInt(answerStr);
-
-      if (answerNumber === sum) {
-        console.log('Correct!');
-      } else {
-        console.log(`${answerNumber} is wrong answer ;(. Correct answer was ${sum}.`);
-        console.log(`Let's try again, ${name}!`);
-        break;
-      }
+      result = number1 - number2;
     }
 
     if (arrOperation[operationIndex] === '*') {
-      const sum = number1 * number2;
-      console.log(`Question: ${number1} * ${number2} `);
+      result = number1 * number2;
+    }
 
-      const answerStr = readlineSync.question('Enter answer:');
-      const answerNumber = parseInt(answerStr);
+    const answerStr = readlineSync.question('Enter answer:');
+    const answerNumber = parseInt(answerStr);
 
-      if (answerNumber === sum) {
-        console.log('Correct!');
-      } else {
-        console.log(`${answerNumber} is wrong answer ;(. Correct answer was ${sum}.`);
-        console.log(`Let's try again, ${name}!`);
-        break;
-      }
+    if (answerNumber === result) {
+      console.log('Correct!');
+    } else {
+      console.log(`${answerNumber} is wrong answer ;(. Correct answer was ${result}.`);
+      console.log(`Let's try again, ${name}!`);
+      break;
     }
 
     if (i === raunds - 1) {
