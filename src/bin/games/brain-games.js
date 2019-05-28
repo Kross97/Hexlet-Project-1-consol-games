f7bad0c1381edf5a814e785b6c4b6c0e6ce029fd
Nikita Bezhenar <kross78080@yandex.ru>
2019-05-28 17:19:00 +0300

    bin1

--- src/bin/games/brain-games.js
+++ src/bin/games/brain-games.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env node
+
 import readlineSync from 'readline-sync';
 import welcome from '../..';
 import even from './brain-even';
@@ -7,47 +7,49 @@ import gcd from './brain-gcd';
 import prog from './brain-progression';
 import prime from './brain-prime';
 
-console.log('===========================');
-console.log('Welcome to the Brain Games!');
-const name = readlineSync.question('May I have your name?: ');
+export default () => {
+  console.log('===========================');
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name?: ');
 
-welcome(name);
+  welcome(name);
 
-const bool = true; //  while uslovie
+  const bool = true; //  while uslovie
 
-while (bool) {
-  console.log('1.Brain-even');
-  console.log('2.Brain-calculyator');
-  console.log('3.Brain-GCD');
-  console.log('4.Brain-progression');
-  console.log('5.Brain-prime');
-  console.log('9.Exit');
+  while (bool) {
+    console.log('1.Brain-even');
+    console.log('2.Brain-calculyator');
+    console.log('3.Brain-GCD');
+    console.log('4.Brain-progression');
+    console.log('5.Brain-prime');
+    console.log('9.Exit');
 
-  const strNumber = readlineSync.question('Enter number game :');
-  const number = parseInt(strNumber, 10);
+    const strNumber = readlineSync.question('Enter number game :');
+    const number = parseInt(strNumber, 10);
 
-  if (number === 1) {
-    even(name);
+    if (number === 1) {
+      even(name);
+    }
+
+    if (number === 2) {
+      calc(name);
+    }
+
+    if (number === 3) {
+      gcd(name);
+    }
+
+    if (number === 4) {
+      prog(name);
+    }
+
+    if (number === 5) {
+      prime(name);
+    }
+    if (number === 9) {
+      console.log('Good bye');
+      console.log('Exit games....');
+      break;
+    }
   }
-
-  if (number === 2) {
-    calc(name);
-  }
-
-  if (number === 3) {
-    gcd(name);
-  }
-
-  if (number === 4) {
-    prog(name);
-  }
-
-  if (number === 5) {
-    prime(name);
-  }
-  if (number === 9) {
-    console.log('Good bye');
-    console.log('Exit games....');
-    break;
-  }
-}
+};
