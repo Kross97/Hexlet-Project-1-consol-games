8a854103f49360a694049fb5c36d057382344761
Kross97 <46231049+Kross97@users.noreply.github.com>
2019-06-03 21:12:42 +0300

    Update progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -2,14 +2,11 @@
 import { rounds, engine } from '../engine/brain-games';
 import random from '../engine/random';
 
-
 export default () => {
   const conditionGame = 'What number is missing in the progression?';
   const gameData = [];
-
   const intervalRandom = 100;
   const intervalRandom2 = 10;
-
   const lengthArr = 10;
 
   for (let i = 0; i < rounds; i += 1) {
